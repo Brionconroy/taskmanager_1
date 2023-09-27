23dedc9a51cdcc94f5032d5aa57e48fb3874bbf6
Brionconroy <razvanbrion@gmail.com>
2023-09-27 16:29:41 +0000

    Add to tasks.html and to the routes files

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,12 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
-    let selects = document.querySelectorAll('select');
+    // select initialization
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
